c7fabebe33f627eb13c0f12b02df59291ec85cee
David <david.cordtz.laebro@gmail.com>
2025-11-12 19:56:34 +0100

    Login/SignUp done

--- 50-shades-of-maple/scripts/gamestate.gd
+++ 50-shades-of-maple/scripts/gamestate.gd
@@ -1,3 +1,22 @@
 extends Node
 
 var soundEffectLevel :float= 0.05
+var chapters = {
+	"Basics" : {
+		"Aritmetik" : {
+			"dependency" : []
+		},
+		"Variabler" : {
+			"dependency" : ["Aritmetik"]
+		}
+	},
+	"Funktioner" : {
+		
+	},
+	"Units" : {
+		
+	}
+}
+const API_URL = "http://139.59.130.153:3000"  
+const API_KEY = "x-api-key: crazyVildAPIKEYIDevelopment!"
+var JWT_TOKEN = ""
--- 50-shades-of-maple/scripts/loginpage.gd
+++ 50-shades-of-maple/scripts/loginpage.gd
@@ -4,15 +4,17 @@ extends Control
 @onready var BlurOverlay = $BlurOverlay
 @onready var SignUpWindow = $SignUpWindow
 @onready var LoginWindow = $LoginWindow
-
-var username = ""
-var password = ""
-var created = false
+@onready var Requests = $HTTPRequest
+@onready var Username = $"SignUpWindow/Username/Type your username"
+@onready var Password = $"SignUpWindow/Password/Type your password"
+@onready var UsernameLogin = $"LoginWindow/Username/Type your username"
+@onready var PasswordLogin = $"LoginWindow/Password/Type your password"
 
 func _ready():
 	queue_redraw()
 	InfoMenu.visible=false
 	BlurOverlay.visible=false
+	Requests.request_completed.connect(_on_request_complete)
 
 #Buttons
 func _on_info_button_pressed() -> void:
@@ -31,6 +33,10 @@ func _on_close_button_pressed() -> void:
 
 func _on_login_button_pressed() -> void:
 	Sound.play_sound("ButtonClicked")
+	var headers = ["Content-Type: application/json", Gamestate.API_KEY]
+	var data = {"username":UsernameLogin.text,"password":PasswordLogin.text}
+	Requests.request(Gamestate.API_URL + "/login",headers,HTTPClient.METHOD_POST,JSON.stringify(data))
+	
 
 func _on_back_to_login_button_pressed() -> void:
 	Sound.play_sound("ButtonClicked")
@@ -49,3 +55,16 @@ func _on_create_account_button_pressed() -> void:
 	Sound.play_sound("ButtonClicked")
 	SignUpWindow.visible=false
 	LoginWindow.visible=true
+	var headers = ["Content-Type: application/json", Gamestate.API_KEY]
+	var data = {"username":Username.text,"password":Password.text}
+	Requests.request(Gamestate.API_URL + "/users",headers,HTTPClient.METHOD_POST,JSON.stringify(data))
+
+func _on_request_complete(result,response_code,headers,body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
+	print(response_code)
+	if json.has("token"):
+		Gamestate.JWT_TOKEN = json["token"]
+		get_tree().change_scene_to_file("res://instances/mainmenu.tscn")
+	else:
+		print("ERROR")
