7dde2e0d14c71ddaa8f017b6a72fc44f7199df67
David <david.cordtz.laebro@gmail.com>
2025-11-12 14:28:27 +0100

    SignUp & Login UI

--- 50-shades-of-maple/scripts/loginpage.gd
+++ 50-shades-of-maple/scripts/loginpage.gd
@@ -2,6 +2,12 @@ extends Control
 
 @onready var InfoMenu = $InfoMenu
 @onready var BlurOverlay = $BlurOverlay
+@onready var SignUpWindow = $SignUpWindow
+@onready var LoginWindow = $LoginWindow
+
+var username = ""
+var password = ""
+var created = false
 
 func _ready():
 	queue_redraw()
@@ -22,3 +28,24 @@ func _on_close_info_button_pressed() -> void:
 func _on_close_button_pressed() -> void:
 	Sound.play_sound("ButtonClicked")
 	get_tree().quit()
+
+func _on_login_button_pressed() -> void:
+	Sound.play_sound("ButtonClicked")
+
+func _on_back_to_login_button_pressed() -> void:
+	Sound.play_sound("ButtonClicked")
+	SignUpWindow.visible=false
+	LoginWindow.visible=true
+
+func _on_go_to_sign_up_pressed() -> void:
+	Sound.play_sound("ButtonClicked")
+	SignUpWindow.visible=true
+	LoginWindow.visible=false
+
+func _on_sign_up_button_pressed() -> void:
+	Sound.play_sound("ButtonClicked")
+
+func _on_create_account_button_pressed() -> void:
+	Sound.play_sound("ButtonClicked")
+	SignUpWindow.visible=false
+	LoginWindow.visible=true
